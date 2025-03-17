abf76bb862599afcab945165f03c02b2321a1df0
Taanishq <taanishqsethi28@gmail.com>
2025-03-17 15:13:56 -0700

    Deleted extra raspberry pi file

--- raspberry-pi/face_uploader.py
+++ raspberry-pi/face_uploader.py
@@ -2,8 +2,12 @@ import requests
 
 SERVER_URL = "http://localhost:8000/verify-face/"
 
-with open("detected_face.jpg", "rb") as image_file:
-    files = {"file": image_file}
-    response = requests.post(SERVER_URL, files=files)
+def upload_image():
+    image_path = "captured_face.jpg"
+    with open(image_path, "rb") as image_file:
+        files = {"file": image_file}
+        response = requests.post(SERVER_URL, files=files)
+        print(response.json())
 
-print(response.json())
+# Upload the captured image
+upload_image()
