97669616a5564b5cb1cb3e6c786c48c75e628574
Taanishq <taanishqsethi28@gmail.com>
2025-04-29 16:08:49 -0700

    Updated config so it uses .env

--- fastapi-backend/config.py
+++ fastapi-backend/config.py
@@ -1,11 +1,10 @@
 # fastapi-backend/config.py
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
+load_dotenv()
 
-#load_dotenv()  # Load environment variables from .env file
-
-#MONGO_URI = os.getenv("MONGO_URI")
+MONGO_URI = os.getenv("MONGO_URI")
 MONGO_URI="mongodb+srv://mongoDB:SeniorDesign@cluster0.yftfhhu.mongodb.net/"
 client    = AsyncIOMotorClient(MONGO_URI)
 db        = client["homesentinel"]
@@ -13,3 +12,10 @@ db        = client["homesentinel"]
 users_collection      = db["users"]
 households_collection = db["households"]
 logs_collection       = db["logs"]
+invites_collection    = db["invites"]
+
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_USER = os.getenv("EMAIL_USER")
+EMAIL_PASS = os.getenv("EMAIL_PASS")
