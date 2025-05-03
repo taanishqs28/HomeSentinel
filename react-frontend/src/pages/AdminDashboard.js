fbd26aeaa9c79dfe83b6231c4889abea5339c45b
Taanishq <taanishqsethi28@gmail.com>
2025-05-03 15:23:26 -0700

    Updated admin dashboard

--- react-frontend/src/pages/AdminDashboard.js
+++ react-frontend/src/pages/AdminDashboard.js
@@ -1,35 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import api from "../api";
 import "../styles/styles.css";
 import "../styles/Dashboard.css";
 
-
 const AdminDashboard = () => {
+  const [firstName, setFirstName] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     const storedUsername = localStorage.getItem("username");
     const role = localStorage.getItem("role");
+
     if (!token || !storedUsername) {
       navigate("/login");
     } else if (role !== "admin") {
-      navigate("/login"); // or navigate("/user-dashboard")
+      navigate("/login");
+    } else {
+      api.get("/auth/me", {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      .then((res) => {
+        const fullName = res.data.name || "";
+        const first = fullName.split(" ")[0];
+        setFirstName(first);
+      })
+      .catch(() => {
+        setFirstName("Admin");
+      });
     }
   }, [navigate]);
-  
 
   return (
     <div className="dashboard-container">
-      <h2>Dashboard</h2>
-      <p>Welcome to HomeSentinel</p>
+      <h2>Admin Dashboard</h2>
+      <p>Welcome to HomeSentinel, {firstName}</p>
       <button onClick={() => navigate("/create-face-recognition")}>Create Face Recognition</button>
       <button onClick={() => navigate("/view-logs")}>View Logs</button>
       <button onClick={() => navigate("/create-household")}>Create Household</button>
       <button onClick={() => navigate("/manage-household")}>Manage Household</button>
-
       <button onClick={() => {
-        localStorage.removeItem("token");
+        localStorage.clear();
         navigate("/login");
       }}>
         Logout
